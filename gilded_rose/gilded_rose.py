38a99998fc382249f2e2d7248b1929aca7acdb40
Ryan Sharp <rysharprules@gmail.com>
2020-08-06 13:33:39 +0100

    Move sulfuras logic into function

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -10,39 +10,44 @@ class GildedRose(object):
     def adjust_quality(self, item, value):
         item.quality = item.quality + value
 
-    def handle_backstage_pass(self, item):
-        if item.name == self.backstage_pass:
-            if item.quality < 50:
-                self.adjust_quality(item, 1)
-            if item.sell_in < 11 and item.quality < 50:
-                self.adjust_quality(item, 1)
-            if item.sell_in < 6 and item.quality < 50:
-                self.adjust_quality(item, 1)
+    def decrement_sell_in(self, item):
+        item.sell_in = item.sell_in - 1
 
-            item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                self.adjust_quality(item, -item.quality)
+    def handle_backstage_pass(self, item):
+        if item.quality < 50:
+            self.adjust_quality(item, 1)
+        if item.sell_in < 11 and item.quality < 50:
+            self.adjust_quality(item, 1)
+        if item.sell_in < 6 and item.quality < 50:
+            self.adjust_quality(item, 1)
+
+        self.decrement_sell_in(item)
+        if item.sell_in < 0:
+            self.adjust_quality(item, -item.quality)
 
     def handle_aged_brie(self, item):
-        if item.name == self.aged_brie:
-            if item.quality < 50:
-                self.adjust_quality(item, 1)
-            item.sell_in = item.sell_in - 1
-            if item.sell_in < 0 and item.quality < 50:
-                self.adjust_quality(item, 1)
+        if item.quality < 50:
+            self.adjust_quality(item, 1)
+        self.decrement_sell_in(item)
+        if item.sell_in < 0 and item.quality < 50:
+            self.adjust_quality(item, 1)
+
+    def handle_sulfuras(self, item):
+        if item.quality < 50:
+            self.adjust_quality(item, 1)
 
     def update_quality(self):
         for item in self.items:
-            self.handle_backstage_pass(item)
-            self.handle_aged_brie(item)
-            if item.name != self.backstage_pass and item.name != self.aged_brie:
-                if item.quality > 0 and item.name != self.sulfuras:
+            if item.name == self.backstage_pass:
+                self.handle_backstage_pass(item)
+            elif item.name == self.aged_brie:
+                self.handle_aged_brie(item)
+            elif item.name == self.sulfuras:
+                self.handle_sulfuras(item)
+            else:
+                if item.quality > 0:
                     self.adjust_quality(item, -1)
-                elif item.quality < 50:
-                    self.adjust_quality(item, 1)
-
-                if item.name != self.sulfuras:
-                    item.sell_in = item.sell_in - 1
-
-                if item.sell_in < 0 and item.quality > 0 and item.name != self.sulfuras:
+                
+                self.decrement_sell_in(item)
+                if item.sell_in < 0 and item.quality > 0:
                     self.adjust_quality(item, -1)
