f0ae6655e0e9287bc496972098820062f3c00760
Ryan Sharp <rysharprules@gmail.com>
2020-08-06 13:19:19 +0100

    Move aged brie logic into function

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -18,29 +18,31 @@ class GildedRose(object):
                 self.adjust_quality(item, 1)
             if item.sell_in < 6 and item.quality < 50:
                 self.adjust_quality(item, 1)
-                
+
             item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 self.adjust_quality(item, -item.quality)
-            
+
+    def handle_aged_brie(self, item):
+        if item.name == self.aged_brie:
+            if item.quality < 50:
+                self.adjust_quality(item, 1)
+            item.sell_in = item.sell_in - 1
+            if item.sell_in < 0 and item.quality < 50:
+                self.adjust_quality(item, 1)
 
     def update_quality(self):
         for item in self.items:
             self.handle_backstage_pass(item)
-            if item.name != self.backstage_pass:
-                if item.name != self.aged_brie and item.quality > 0 and item.name != self.sulfuras:
+            self.handle_aged_brie(item)
+            if item.name != self.backstage_pass and item.name != self.aged_brie:
+                if item.quality > 0 and item.name != self.sulfuras:
                     self.adjust_quality(item, -1)
-                else:
-                    if item.quality < 50:
-                        self.adjust_quality(item, 1)
+                elif item.quality < 50:
+                    self.adjust_quality(item, 1)
 
                 if item.name != self.sulfuras:
                     item.sell_in = item.sell_in - 1
 
-                if item.sell_in < 0:
-                    if item.name != self.aged_brie:
-                        if item.quality > 0 and item.name != self.sulfuras:
-                            self.adjust_quality(item, -1)
-                    else:
-                        if item.quality < 50:
-                            self.adjust_quality(item, 1)
+                if item.sell_in < 0 and item.quality > 0 and item.name != self.sulfuras:
+                    self.adjust_quality(item, -1)
